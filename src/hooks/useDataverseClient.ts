7231c2f8252b48490831c24087f20c8662b67f55
Scott Durow <scott_durow@hotmail.com>
2022-07-20 16:09:03 -0700

    fix: updated to dataverse-ify@2.0.3

--- src/hooks/useDataverseClient.ts
+++ src/hooks/useDataverseClient.ts
@@ -1,18 +1,18 @@
 import { useAccount, useMsal } from '@azure/msal-react';
 import { XrmContextDataverseClient } from 'dataverse-ify';
-import { SPAWebApi } from 'dataverse-ify/lib/webapi/spa';
+import { BrowserWebApi } from 'dataverse-ify/lib/webapi/browser';
 import { useEffect, useRef } from 'react';
 import { silentRequest } from '../authConfig';
 import { dataverseConfig } from '../dataverseConfig';
 export const useDataverseClient = (operation: (client: XrmContextDataverseClient) => void, dependencies: unknown[]) => {
     const dataverseClientRef = useRef<XrmContextDataverseClient>();
-    const webApiRef = useRef<SPAWebApi>();
+    const webApiRef = useRef<BrowserWebApi>();
     const { instance, accounts } = useMsal();
     const account = useAccount(accounts[0]);
 
     useEffect(() => {
         if (!dataverseClientRef.current) {
-            const webApi = new SPAWebApi(dataverseConfig.server, dataverseConfig.version);
+            const webApi = new BrowserWebApi(dataverseConfig.server, dataverseConfig.version);
             const dataverseClient = new XrmContextDataverseClient(webApi as unknown as Xrm.WebApi);
             dataverseClientRef.current = dataverseClient;
             webApiRef.current = webApi;
